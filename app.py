dddce57d91401d2332a535fc5314e7c84af91c90
Portside <108273389+PortsideLearn@users.noreply.github.com>
2023-06-07 22:53:33 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
+# importes
 from flask import Flask, render_template, request, redirect, url_for, session
 from jinja2 import Template
 import random
 
+# settings app
 app = Flask(__name__)
 app.secret_key = 'your secret key'
 app.static_folder = 'static'
 
+# create a main page
 @app.route('/')
 def index():
+    # create random news for html page
     news = random.choice(['Putin dont prisedent', 
                           'Zirinovski is dead', 
                           'Portside is best programmer'])
-    return render_template('index.html', news=news)
+    return render_template('index.html', news=news) # render html page 
   
-  
+  # start application
   if __name__ == "__main__":
       app.run(debug=True, port=80)
+     # port example: "127.0.0.1:port"
+     # debug is - if your app returned error, flask return error to web-site
